b9e538ff0c44d8a1657fd286ab89657a539fd896
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-01 14:07:02 -0800

    added second test and refactored to dot notation

--- function-1/function1.test.js
+++ function-1/function1.test.js
@@ -4,5 +4,11 @@ describe('get name function', () => {
   it('test will confirm if function returns name property from an object', () => {
     const object = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' }
     expect(getName(object)).toEqual('Aang');
-  });  
+  });
+
+  it('test will confirm if function returns name property from an object', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' }
+    expect(getName(spot)).toEqual('spot');
+  });
+
 });
