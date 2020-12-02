a607d5289b56c126c8ed9f39d83b8503da6724f9
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-01 16:48:46 -0800

    updated function4 test using mocks example from demo

--- function-4/function4.test.js
+++ function-4/function4.test.js
@@ -1,8 +1,25 @@
 const request = require('superagent');
 const { fetchQuotes } = require('./function4.js');
+jest.mock('superagent');
+
 
 describe('Test verifies that function returns a single quote with expected format', () => {
   it('Test verifies that function returns a single quote with expected format', async () => {
+    request.get.mockResolvedValue({
+        body: [
+            {
+                character: expect.anything(),
+                quote: expect.anything(),
+                image: expect.anything()   
+            },
+            {
+                character: expect.anything(),
+                quote: expect.anything(),
+                image: expect.anything()   
+            }
+        ]
+    })
+
     const objectFormat = {
         character: expect.anything(),
         quote: expect.anything(),
