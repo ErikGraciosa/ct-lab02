7c03267330262a5d629cd91ea23d4b80c1ce0f5d
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-01 17:00:40 -0800

    corrected refactor issue in function4, tests now passing

--- function-4/function4.js
+++ function-4/function4.js
@@ -1,9 +1,9 @@
 const request = require('superagent');
 
 const fetchQuotes = async () => {
-    const quotes = await request.get('https://futuramaapi.herokuapp.com/api/characters/Dr-Zoidberg')
-    const randNum = quotes.body.length * Math.random();
-    return quotes.body[Math.floor(randNum)];
+    const { body } = await request.get('https://futuramaapi.herokuapp.com/api/characters/Dr-Zoidberg')
+    const randNum = body.length * Math.random();
+    return body[Math.floor(randNum)];
 }
 
 module.exports = { fetchQuotes }
